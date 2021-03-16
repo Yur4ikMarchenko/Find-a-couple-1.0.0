cecd33c7768a9a0e48e996e5b6b5ed616856c8f3
Yur4ikMarchenko <marchenko_ak19@nuwm.edu.ua>
2021-03-16 14:04:48 +0200

    fix builds

--- Find a couple/Assets/Scripts/CardManager.cs
+++ Find a couple/Assets/Scripts/CardManager.cs
@@ -1,10 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEditor;
-using UnityEditor.Animations;
 
 public class CardManager : MonoBehaviour
 {
