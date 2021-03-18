93a492a6286c9e621b7a367157fce4479b473bfe
randomanimedude <koval_ak19@nuwm.edu.ua>
2021-03-18 16:18:59 +0200

    last lvl victory text

---
+++ Find a couple/Assets/Scripts/VictoryText.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VictoryText : MonoBehaviour
+{
+    void Awake()
+    {
+        GetComponent<Text>().text = (LevelManager.IsLastLevel()) ? "You've completed the game!" : "Nice!";
+    }
+}
