b27031234189fb69bc8ee6e7909f4bf80583091b
randomanimedude <koval_ak19@nuwm.edu.ua>
2021-03-18 14:48:55 +0200

    dark theme state save

--- Find a couple/Assets/Scripts/OptionsLoader.cs
+++ Find a couple/Assets/Scripts/OptionsLoader.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class OptionsLoader : MonoBehaviour
 {
+    public GameObject fon;
+    public GameObject darkFon;
     void Awake()
     {
         LevelManager.LoadLevelProgress();
@@ -13,6 +15,13 @@ public class OptionsLoader : MonoBehaviour
 
         if (PlayerPrefs.HasKey("IsMusicMuted"))
             Options.isMusicMuted = (PlayerPrefs.GetInt("IsMusicMuted") == 1) ? true : false;
+
+        if (PlayerPrefs.HasKey("DarkTheme"))
+        {
+            Options.darkTheme = (PlayerPrefs.GetInt("DarkTheme") == 1) ? true : false;
+            darkFon.gameObject.SetActive(Options.darkTheme);
+            fon.gameObject.SetActive(!Options.darkTheme);
+        }
     }
 
 }
