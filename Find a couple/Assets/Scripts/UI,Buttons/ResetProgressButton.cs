caeb9ccccb3518e45f1b63735a3a068cc4d408be
randomanimedude <koval_ak19@nuwm.edu.ua>
2021-04-04 12:17:17 +0300

    new buttons + bg fixes

--- Find a couple/Assets/Scripts/UI,Buttons/ResetProgressButton.cs
+++ Find a couple/Assets/Scripts/UI,Buttons/ResetProgressButton.cs
@@ -1,11 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResetProgressButton : MonoBehaviour
 {
+    Color active = new Color(1, 1, 1, 1);
+    Color inActive = new Color(1, 1, 1, 0.7f);
+
+    private void Start()
+    {
+        UpdateVisual();
+    }
+
+    void UpdateVisual()
+    {
+        GetComponent<Image>().color = LevelManager.IsLevelAvailable(1) ? active : inActive;
+    }
+
     public void ResetProgress()
     {
         LevelManager.ResetProgress();
+        UpdateVisual();
     }
 }
